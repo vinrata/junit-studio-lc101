bf733330c023916ebdc10bda8473d68ec640ebef
vinrata <vinnycamma@gmail.com>
2021-09-17 11:06:55 -0500

    added 9 tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -3,7 +3,6 @@ package test;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 public class BalancedBracketsTest {
 
     //TODO: add tests here
@@ -12,5 +11,47 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void bracketTestTwoBrackets(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[]"));
 
+    }
+    @Test
+    public void bracketTestWithLaunchCode(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+
+    }
+    @Test
+    public void bracketTestWithLaunchandCodeInBrackets(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+
+    }
+    @Test
+    public void bracketTestEmptyString(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets(""));
+
+    }
+    @Test
+    public void bracketTestFalseLaunchCodeInBrackets(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+
+    }
+
+    @Test
+    public void bracketTestFalseLaunchCodeCloseBracketCodeOpenBracket(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+
+    }
+
+    @Test
+    public void bracketTestFalseOneOpenBracket(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("["));
+
+    }
+
+    @Test
+    public void bracketTestFalseBackwardsBrackets(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("]["));
+
+    }
 }
